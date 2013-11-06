3e2355665c132b6e555be34ea175d5ff5197baa6
Martin Seener <martin.seener@barzahlen.de>
2013-11-06 12:05:48 +0100

    changed the way, brickd is installed to a shorter more precise one

--- manifests/install/base.pp
+++ manifests/install/base.pp
@@ -15,25 +15,21 @@
 # Copyright 2013 Martin Seener
 #
 class tinkerforge::install::base {
-  package { 'brickd':
-    ensure  => present,
-    require => [ Package['libusb-1.0-0'], Exec['get-brickd'] ],
-  }
-  
+  package { 'libudev0':
+    ensure => present,
+  } ->
+
   package { 'libusb-1.0-0':
     ensure  => present,
-    require => Package['libudev0'],
-  }
+  } ->
 
-  package { 'libudev0':
-    ensure  => present,
-    notify  => Exec['get-brickd'],
-  }
-  
   exec { 'get-brickd':
     command     => 'wget http://download.tinkerforge.com/tools/brickd/linux/brickd_linux_latest_amd64.deb -O /tmp/brickd.deb && dpkg -i /tmp/brickd.deb',
     onlyif      => 'test ! -f /tmp/brickd.deb',
-    refreshonly => true,
     timeout     => 0,
+  } ->
+
+  package { 'brickd':
+    ensure  => present,
   }
 }
